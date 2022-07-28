0dc8af74d23690b2f7200266168d920781217784
Steve Zatz <slzatz@gmail.com>
2022-07-28 08:58:25 -0400

    main.go: added more button actions but still thinking about best approach

--- main.go
+++ main.go
@@ -38,9 +38,28 @@ var (
 		1:  "shuffle patty griffin",
 		2:  "shuffle aimee mann",
 		3:  "shuffle amy winehouse",
+		4:  "album late for the sky jackson browne",
+		5:  "album mental illness aimee mann",
+		6:  "album after the goldrush neil young",
+		7:  "album living in clip ani difranco",
+		8:  "playlist Mortality",
+		9:  "playlist Every Single Day",
+		10: "station patty griffin",
+		11: "station all you have is your soul",
+		14: "next",
 		15: "play_pause",
 	}
 )
+
+/*Amazon My Music Playlists
+An Unarmorial Age
+Anything that Can't Be Left Behind
+Dec. 29, 2012
+December 2015
+Every Single Day
+Like You've Never Seen Water
+Mortality
+*/
 
 func main() {
 	time.Sleep(5 * time.Second)
